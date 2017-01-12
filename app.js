3786bb66b9a654dfad8a6a669b64c8181ade3ba0
sachin <sachin@zoyo.co.in>
2017-01-12 16:58:22 +0530

    first

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express();
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'startbootstrap-agency-gh-pages')));
+
+app.get('/', function(req, res, next){
+  res.send(__dirname, 'public/startbootstrap-agency-gh-pages' , 'index.html');
+});
+
+app.listen(3000, function () {
+
+})
